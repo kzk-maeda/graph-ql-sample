37b76106f1c569141458c2b23539edbf673f6aeb
kzk-maeda <kzk.maeda0711@gmail.com>
2020-11-23 22:39:50 +0900

    add new schema and mutation

--- graphql/server/schema/schema.js
+++ graphql/server/schema/schema.js
@@ -1,7 +1,8 @@
 const graphql = require('graphql')
 const Movie = require('../models/movie')
+const Director = require('../models/director')
 
-const { GraphQLObjectType, GraphQLID, GraphQLString } = graphql
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt } = graphql
 
 const MovieType = new GraphQLObjectType({
     name: 'Movie',
@@ -12,19 +13,70 @@ const MovieType = new GraphQLObjectType({
     })
 })
 
+const DirectorType = new GraphQLObjectType({
+    name: 'Director',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type:GraphQLString},
+        age: {type: GraphQLInt}
+    })
+})
+
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
         movie: {
             type: MovieType,
-            args: {id:{type:GraphQLString}},
+            args: {id:{type:GraphQLID}},
             resolve(parents, args){
                 return Movie.findById(args.id)
+            }
+        },
+        director: {
+            type: DirectorType,
+            args: {id:{type:GraphQLID}},
+            resolve(parents, args){
+                return Director.findById(args.id)
+            }
+        }
+    }
+})
+
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addMovie: {
+            type: MovieType,
+            args: {
+                name: {type:GraphQLString},
+                genre: {type:GraphQLString}
+            },
+            resolve(parent, args) {
+                let movie = new Movie({
+                    name: args.name,
+                    genre: args.genre
+                })
+                return movie.save()
+            }
+        },
+        addDirector: {
+            type: DirectorType,
+            args: {
+                name: {type:GraphQLString},
+                age: {type:GraphQLInt}
+            },
+            resolve(parent, args) {
+                let director = new Director({
+                    name: args.name,
+                    age: args.age
+                })
+                return director.save()
             }
         }
     }
 })
 
 module.exports = new graphql.GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 })
