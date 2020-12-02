e961b942af1a61f9139f0a321df7a84bb7295e30
kzk-maeda <kzk.maeda0711@gmail.com>
2020-12-03 02:17:58 +0900

    add new date via GraphQL

--- graphql/client/src/queries/queries.js
+++ graphql/client/src/queries/queries.js
@@ -11,3 +11,19 @@ export const MOVIE_LIST = gql`
             }
         }
     }`
+
+export const DIRECTOR_LIST = gql`
+    query {
+        directors{
+            id
+            name
+        }
+    }`
+
+export const ADD_MOVIE = gql`
+    mutation($name: String!, $genre: String!, $directorId: ID!) {
+        addMovie(name: $name genre: $genre directorId: $directorId) {
+        name
+        genre
+        }
+    }`
